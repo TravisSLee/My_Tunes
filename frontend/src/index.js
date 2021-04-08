e45d7dfc9ddad6ef13cb6a7100ff15f7e5501ca9
TravisSLee <travisslee223@gmail.com>
2021-04-08 15:23:58 -0400

    update frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'; 
+import songsReducer from './reducers/songsReducer';
 
+const store = createStore(songsReducer, applyMiddleware(thunk))
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/reducers/songsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    songs: [],
+    loading: true
+}
+
+const songsReducer = (state, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
+
+export default songsReducer;
