f988ebf62f75778b64ff5d0db30a91bf0e31a9de
TravisSLee <travisslee223@gmail.com>
2021-04-13 15:35:34 -0400

    limit routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :songs
-  resources :artists
+  resources :artists, except: [:update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
